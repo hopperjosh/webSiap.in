7fce929644d093ba0773bae129b746644c09402f
whoever_is <dimyawan2@gmail.com>
2025-08-03 07:26:53 +0700

    a

--- haha.py
+++ haha.py
@@ -1,18 +1,35 @@
-import numpy as np
+import os
+import requests
 
-print("numpy berhasil diimpor:", np.__version__)
-print("contoh array numpy:", np.array([1, 2, 3, 4, 5]))
+API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev"
+headers = {
+    "Authorization": f"Bearer {os.environ['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']}",
+}
 
-a = np.array([1, 2, 3])
-b = np.array([4, 5,6])
-print("hasil penjumlahan a + b:", a + b)
-print("hasil penjumlahan a + b:", a - b)
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
 
-import torch
-print("CUDA available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("GPU:", torch.cuda.get_device_name(0))
+image_bytes = query({
+    "inputs": "Astronaut riding a horse",
+})
 
-print("hasil perkalian a * b:", a * b)
-print("hasil pembagian a / b:", a / b)
-print("sudah selesai menjalankan haha.py")
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+
+
+from huggingface_hub import InferenceClient
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=os.environ["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"],
+)
+
+# output is a PIL.Image object
+image = client.text_to_image(
+    "Astronaut riding a horse",
+    model="black-forest-labs/FLUX.1-dev",
+)
+
